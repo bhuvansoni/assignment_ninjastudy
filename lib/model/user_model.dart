c4f993a55436442f203d90fb890b905640d2b755
bhuvansoni <bp8340@gmail.com>
2022-05-16 12:13:38 +0530

    added chat screen, sign up page and dashboard

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,6 +1,17 @@
 class UserModel {
-  String email;
-  String password;
+  final String uid;
+  final String email;
+  final String name;
 
-  UserModel({required this.email, required this.password});
+  UserModel({required this.uid, required this.email, required this.name});
+
+  factory UserModel.fromMap(Map data) {
+    return UserModel(
+      uid: data['uid'],
+      email: data['email'] ?? '',
+      name: data['name'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() => {"uid": uid, "email": email, "name": name};
 }
